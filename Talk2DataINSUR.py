ad4061490bca84dcee4e17c9df0369dd4e96b5b8
Ravivarman Balaiyan <73479891+ravivarmanb@users.noreply.github.com>
2024-12-03 17:57:07 +0530

    Update Talk2DataINSUR.py

--- Talk2DataINSUR.py
+++ Talk2DataINSUR.py
@@ -172,8 +172,8 @@ def main():
             answer = second_llm_call(llm = llm, sql_response=query_results,user_question = user_question)
             
             st.write(answer)
-            st.write(sql_query)
-            st.write(query_results)
+            #st.write(sql_query)
+            #st.write(query_results)
             st.success("Done")
 
 if __name__ == "__main__":
